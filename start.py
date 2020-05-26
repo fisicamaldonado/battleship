32dc828bb58a1ee1fe5e152ed5198546f642aa15
fisicamaldonado <fisicamaldonado.vaz@gmail.com>
2020-05-26 01:26:39 -0300

    Update start.py

--- start.py
+++ start.py
@@ -1,10 +1,10 @@
 from random import randint 
 
 def create_board():
-    board=[]
-    for j in range(5):
-        board.append(["O"]*5)
-    return board
+  board=[]
+  for j in range(5):
+    board.append(["O"]*5)
+  return board
 
 board=create_board()
 
@@ -29,25 +29,25 @@ ship_col = random_col(board)
 
 #Guessing where is our ship
 for turn in range(5):
-    print ("Turn ",turn+1)
-    guess_row = int(input("Input your Guess Row: "))
-    guess_col = int(input("Input your Guess Col: "))
+  print ("Turn ",turn+1)
+  guess_row = int(input("Input your Guess Row: "))
+  guess_col = int(input("Input your Guess Col: "))
 
-    if guess_row == ship_row and guess_col == ship_col:
-        print ("Congratulations! You hit the ship!")
-        break
+  if guess_row == ship_row and guess_col == ship_col:
+    print ("Congratulations! You hit the ship!")
+    break
+  else:
+    if guess_row not in range(5) or \
+    guess_col not in range(5):
+      print ("Hey! That's out of board's range.")
+    elif board[guess_row][guess_col] == "X":
+      print ("You already guessed that!")
     else:
-        if guess_row not in range(5) or \
-        guess_col not in range(5):
-            print ("Hey! That's out of board's range.")
-        elif board[guess_row][guess_col] == "X":
-            print ("You already guessed that!")
-        else:
-            print ("Sorry, you missed the ship!")
-            board[guess_row][guess_col]="X"
-    print_board(board)
-    if turn == 4:
-      print ("Game Over")
-      break
-    else:
-      print ("Try again!)
+      print ("Sorry, you missed the ship!")
+      board[guess_row][guess_col]="X"
+  print_board(board)
+  if turn == 4:
+    print ("Game Over")
+    break
+  else:
+    print ("Try again!")
